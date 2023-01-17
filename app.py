f6007235e872c5038e95fa0f637dbd0e61e09e0a
HenryNunes <henry.nunes@outlook.com>
2023-01-17 19:39:15 -0300

    update

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ import cv2
 
 pontosOriginal = []
 pontosNova = []
+imgO = None #Imagem Original
+imgN = None #Imagem Nova
 
 def abrirImg(nome):
     return cv2.imread(nome)
 
 def homography_matrix(src_points, dst_points):
     pass
+
+
 
 def clicar(event, x, y, flags, params):
     if event == cv2.EVENT_LBUTTONDOWN:
@@ -23,6 +27,11 @@ def clicar(event, x, y, flags, params):
                 print(ponto[0], ' ', ponto[1])
             coletarPontosNovos()
             matriz = homography_matrix(pontosOriginal, pontosNova)
+            imgN = gerarImagem(imgO, matriz)
+            cv2.imshow("Imagem Nova", imgN)
+            cv2.imwrite("ImagemNova.jpg",imgN)
+            
+
         
             
 def coletarPontosNovos():
@@ -34,14 +43,17 @@ def coletarPontosNovos():
     for ponto in pontosNova:
                 print(ponto[0], ' ', ponto[1])
 
+def gerarImagem(img, matriz):
+    pass
 
 
 def main():
     path = input("Digite o nome do arquivo:\n")
-    img = abrirImg(path)
+    imgO = abrirImg(path)
     nomeOriginal = "Imagem Original"
-    cv2.imshow(nomeOriginal, img)
+    cv2.imshow(nomeOriginal, imgO)
     cv2.setMouseCallback(nomeOriginal, clicar)
+    #segue do evento clicar
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
